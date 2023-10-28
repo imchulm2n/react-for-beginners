b4ff6f3e26d173cbeaf6f485befec1209f8ae585
imchulm2n <110436172+imchulm2n@users.noreply.github.com>
2023-10-28 12:22:20 +0900

    수정4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Detail from "./routes/Detail";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route
-          path={process.env.PUBLIC_URL + "/movie/:id"}
+          path={`${process.env.PUBLIC_URL}` + "/movie/:id"}
           element={<Detail />}
         />
-        <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
       </Switch>
     </Router>
   );
